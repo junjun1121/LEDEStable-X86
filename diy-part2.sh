2d126957ba1e450884bad8de45d3065cf6b412e1
junjun1121 <136947888+junjun1121@users.noreply.github.com>
2023-09-12 14:11:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,4 +22,4 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/psw
+git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
